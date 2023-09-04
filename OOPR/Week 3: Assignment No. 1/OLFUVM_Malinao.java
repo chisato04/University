593dfa55e4ebb502f5b59f1d2c44892b4d21d412
chisao04 <jamesmalinao15@gmail.com>
2023-09-04 19:12:44 +0800

    –allow-empty-message

---
+++ OOPR/Week 3: Assignment No. 1/OLFUVM_Malinao.java
@@ -0,0 +1,33 @@
+public class OLFUVM_Malinao
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("\t\t\t\t\tOUR LADY OF FATIMA UNIVERSITY VISION");
+        System.out.println("A premier inclusive university of choice aspiring to improve man as man by developing individuals through a legacy of excellent");
+        System.out.println("\t\t\t\t\teducation and compassionate value formation");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("\t\t\t\t\tOUR LADY OF FATIMA UNIVERSITY MISSION");
+        System.out.println("");
+        System.out.println("\t\t\tThe Our Lady of Fatima University, together with Fatima Medical Science");
+        System.out.println("\t\t\s\s\s\sFoundation, Inc., is dedicated to the improvement of man as man through holistic");
+        System.out.println("\t\t\tformation of individuals imbued with knowledge, skills and virtues");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("\t\t\t\t\tCOLLEGE OF COMPUTER SCIENCE VISION");
+        System.out.println("");
+        System.out.println("\t\t\tWe are committed to provide accessible, responsive, and quality");
+        System.out.println("\t\t\t\sInformation Technology Education (ITE) programs and to become");
+        System.out.println("\t\t\s\s\s\s\s\sthe Institution of choice in producing competent and responsible IT");
+        System.out.println("\t\t\tprofessionals who are sensitive to the needs and demands of the");
+        System.out.println("\t\t\t\t\t\t\s\s\s\sindustry");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("\t\t\t\t\tCOLLEGE OF COMPUTER STUDIES MISSION");
+        System.out.println("");
+        System.out.println("\t\t\tThe College of Computer Studies aims to provide innovative and quality");
+        System.out.println("\t\t\t\s\sinstruction to the advancement of technology intends to develop an");
+        System.out.println("entrepreneurial learning environment towards sustainability and growth; and develops resposive and morally upright citizens.");
+    }
+
+}
