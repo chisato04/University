a0e8094ae2545e34c543a97743b9008d32e60e68
chisato <jamesmalinao15@gmail.com>
2023-09-12 22:48:27 +0800

    update

--- OOPR211/Laboratory Assignment No. 2/LabAct2_Malinao.java
+++ OOPR211/Laboratory Assignment No. 2/LabAct2_Malinao.java
@@ -2,14 +2,20 @@ public class LabAct2_Malinao
 {
     public static void main(String[] args)
     {
-        int n1=5, n2=20, swap; //defining variables
+        //defining variables
+        int n1=5, 
+            n2=20, 
+            swap;
+            
         System.out.println("Values before exchange");
         System.out.println("value of n1: " + n1);
         System.out.println("value of n2: " + n2);
 
-        swap=n1; //defining process
+        //defining process
+        swap=n1; 
         n1=n2;
         n2=swap;
+
         System.out.println("Values after exchange");
         System.out.println("value of n1: " + n1);
         System.out.println("value of n2: " + swap);
